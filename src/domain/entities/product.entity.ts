a9d94a1a0cafd06a1f2c641b7471cfaaad7f8b9b
qaws7791 <qaws7791@gmail.com>
2024-12-01 05:01:25 +0900

    feat: 제품 엔티티에 이미지 및 날짜 필드 추가, 가격 텍스트 포매팅 함수 구현

--- src/domain/entities/product.entity.ts
+++ src/domain/entities/product.entity.ts
@@ -1,6 +1,7 @@
 export type ProductId = number;
 export type ProductName = string;
 export type ProductDescription = string;
+export type ProductImage = string;
 export type ProductPrice = number;
 export type ProductStock = number;
 
@@ -8,6 +9,16 @@ export type Product = {
   id: ProductId;
   name: ProductName;
   description: ProductDescription;
+  image: ProductImage;
   price: ProductPrice;
   stock: ProductStock;
+  createdAt: DateTimeString;
+  updatedAt: DateTimeString;
 };
+
+export function getPriceText(product: Product) {
+  return new Intl.NumberFormat("ko-KR", {
+    style: "currency",
+    currency: "KRW",
+  }).format(product.price);
+}
